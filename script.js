388c78e16bc9b50d50b4dc211bdbcec2d659f547
Jo Hill <jos.a.hill@gmail.com>
2022-01-27 22:35:32 -0800

    updated html and js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Client credentials
-var id = 'MwpgXWarnvcadjhT4uZd88o9pn3qvRJi';
-var secret = 'EFAoHdp6QWQXMXam';
+var id = 'eoA8v8C7DRAGuCpveUVcpkQ3ufQfyrBb';
+var secret = 'mANKYSsdT5JsART3';
 var areaEl = document.querySelector('#area')
 var areavaxEl = document.querySelector('#areavax')
 var areavax2El = document.querySelector('#areavax2')
@@ -9,82 +9,344 @@ var infectionsEl = document.querySelector('#infections')
 var riskEl = document.querySelector('#riskLevel')
 var typeEl = document.querySelector('#type')
 var summaryEl = document.querySelector('#summary')
+var countryEl = document.getElementById("country")
+
+
+var countryCode = {
+    "AF": "Afghanistan",
+    "AL": "Albania",
+    "DZ": "Algeria",
+    "AS": "American Samoa",
+    "AD": "Andorra",
+    "AO": "Angola",
+    "AI": "Anguilla",
+    "AQ": "Antarctica",
+    "AG": "Antigua and Barbuda",
+    "AR": "Argentina",
+    "AM": "Armenia",
+    "AW": "Aruba",
+    "AU": "Australia",
+    "AT": "Austria",
+    "AZ": "Azerbaijan",
+    "BS": "Bahamas (the)",
+    "BH": "Bahrain",
+    "BD": "Bangladesh",
+    "BB": "Barbados",
+    "BY": "Belarus",
+    "BE": "Belgium",
+    "BZ": "Belize",
+    "BJ": "Benin",
+    "BM": "Bermuda",
+    "BT": "Bhutan",
+    "BO": "Bolivia (Plurinational State of)",
+    "BQ": "Bonaire, Sint Eustatius and Saba",
+    "BA": "Bosnia and Herzegovina",
+    "BW": "Botswana",
+    "BV": "Bouvet Island",
+    "BR": "Brazil",
+    "IO": "British Indian Ocean Territory (the)",
+    "BN": "Brunei Darussalam",
+    "BG": "Bulgaria",
+    "BF": "Burkina Faso",
+    "BI": "Burundi",
+    "CV": "Cabo Verde",
+    "KH": "Cambodia",
+    "CM": "Cameroon",
+    "CA": "Canada",
+    "KY": "Cayman Islands (the)",
+    "CF": "Central African Republic (the)",
+    "TD": "Chad",
+    "CL": "Chile",
+    "CN": "China",
+    "CX": "Christmas Island",
+    "CC": "Cocos (Keeling) Islands (the)",
+    "CO": "Colombia",
+    "KM": "Comoros (the)",
+    "CD": "Congo (the Democratic Republic of the)",
+    "CG": "Congo (the)",
+    "CK": "Cook Islands (the)",
+    "CR": "Costa Rica",
+    "HR": "Croatia",
+    "CU": "Cuba",
+    "CW": "Curaçao",
+    "CY": "Cyprus",
+    "CZ": "Czechia",
+    "CI": "Côte d'Ivoire",
+    "DK": "Denmark",
+    "DJ": "Djibouti",
+    "DM": "Dominica",
+    "DO": "Dominican Republic (the)",
+    "EC": "Ecuador",
+    "EG": "Egypt",
+    "SV": "El Salvador",
+    "GQ": "Equatorial Guinea",
+    "ER": "Eritrea",
+    "EE": "Estonia",
+    "SZ": "Eswatini",
+    "ET": "Ethiopia",
+    "FK": "Falkland Islands (the) [Malvinas]",
+    "FO": "Faroe Islands (the)",
+    "FJ": "Fiji",
+    "FI": "Finland",
+    "FR": "France",
+    "GF": "French Guiana",
+    "PF": "French Polynesia",
+    "TF": "French Southern Territories (the)",
+    "GA": "Gabon",
+    "GM": "Gambia (the)",
+    "GE": "Georgia",
+    "DE": "Germany",
+    "GH": "Ghana",
+    "GI": "Gibraltar",
+    "GR": "Greece",
+    "GL": "Greenland",
+    "GD": "Grenada",
+    "GP": "Guadeloupe",
+    "GU": "Guam",
+    "GT": "Guatemala",
+    "GG": "Guernsey",
+    "GN": "Guinea",
+    "GW": "Guinea-Bissau",
+    "GY": "Guyana",
+    "HT": "Haiti",
+    "HM": "Heard Island and McDonald Islands",
+    "VA": "Holy See (the)",
+    "HN": "Honduras",
+    "HK": "Hong Kong",
+    "HU": "Hungary",
+    "IS": "Iceland",
+    "IN": "India",
+    "ID": "Indonesia",
+    "IR": "Iran (Islamic Republic of)",
+    "IQ": "Iraq",
+    "IE": "Ireland",
+    "IM": "Isle of Man",
+    "IL": "Israel",
+    "IT": "Italy",
+    "JM": "Jamaica",
+    "JP": "Japan",
+    "JE": "Jersey",
+    "JO": "Jordan",
+    "KZ": "Kazakhstan",
+    "KE": "Kenya",
+    "KI": "Kiribati",
+    "KP": "Korea (the Democratic People's Republic of)",
+    "KR": "Korea (the Republic of)",
+    "KW": "Kuwait",
+    "KG": "Kyrgyzstan",
+    "LA": "Lao People's Democratic Republic (the)",
+    "LV": "Latvia",
+    "LB": "Lebanon",
+    "LS": "Lesotho",
+    "LR": "Liberia",
+    "LY": "Libya",
+    "LI": "Liechtenstein",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "MO": "Macao",
+    "MG": "Madagascar",
+    "MW": "Malawi",
+    "MY": "Malaysia",
+    "MV": "Maldives",
+    "ML": "Mali",
+    "MT": "Malta",
+    "MH": "Marshall Islands (the)",
+    "MQ": "Martinique",
+    "MR": "Mauritania",
+    "MU": "Mauritius",
+    "YT": "Mayotte",
+    "MX": "Mexico",
+    "FM": "Micronesia (Federated States of)",
+    "MD": "Moldova (the Republic of)",
+    "MC": "Monaco",
+    "MN": "Mongolia",
+    "ME": "Montenegro",
+    "MS": "Montserrat",
+    "MA": "Morocco",
+    "MZ": "Mozambique",
+    "MM": "Myanmar",
+    "NA": "Namibia",
+    "NR": "Nauru",
+    "NP": "Nepal",
+    "NL": "Netherlands (the)",
+    "NC": "New Caledonia",
+    "NZ": "New Zealand",
+    "NI": "Nicaragua",
+    "NE": "Niger (the)",
+    "NG": "Nigeria",
+    "NU": "Niue",
+    "NF": "Norfolk Island",
+    "MP": "Northern Mariana Islands (the)",
+    "NO": "Norway",
+    "OM": "Oman",
+    "PK": "Pakistan",
+    "PW": "Palau",
+    "PS": "Palestine, State of",
+    "PA": "Panama",
+    "PG": "Papua New Guinea",
+    "PY": "Paraguay",
+    "PE": "Peru",
+    "PH": "Philippines (the)",
+    "PN": "Pitcairn",
+    "PL": "Poland",
+    "PT": "Portugal",
+    "PR": "Puerto Rico",
+    "QA": "Qatar",
+    "MK": "Republic of North Macedonia",
+    "RO": "Romania",
+    "RU": "Russian Federation (the)",
+    "RW": "Rwanda",
+    "RE": "Réunion",
+    "BL": "Saint Barthélemy",
+    "SH": "Saint Helena, Ascension and Tristan da Cunha",
+    "KN": "Saint Kitts and Nevis",
+    "LC": "Saint Lucia",
+    "MF": "Saint Martin (French part)",
+    "PM": "Saint Pierre and Miquelon",
+    "VC": "Saint Vincent and the Grenadines",
+    "WS": "Samoa",
+    "SM": "San Marino",
+    "ST": "Sao Tome and Principe",
+    "SA": "Saudi Arabia",
+    "SN": "Senegal",
+    "RS": "Serbia",
+    "SC": "Seychelles",
+    "SL": "Sierra Leone",
+    "SG": "Singapore",
+    "SX": "Sint Maarten (Dutch part)",
+    "SK": "Slovakia",
+    "SI": "Slovenia",
+    "SB": "Solomon Islands",
+    "SO": "Somalia",
+    "ZA": "South Africa",
+    "GS": "South Georgia and the South Sandwich Islands",
+    "SS": "South Sudan",
+    "ES": "Spain",
+    "LK": "Sri Lanka",
+    "SD": "Sudan (the)",
+    "SR": "Suriname",
+    "SJ": "Svalbard and Jan Mayen",
+    "SE": "Sweden",
+    "CH": "Switzerland",
+    "SY": "Syrian Arab Republic",
+    "TW": "Taiwan",
+    "TJ": "Tajikistan",
+    "TZ": "Tanzania, United Republic of",
+    "TH": "Thailand",
+    "TL": "Timor-Leste",
+    "TG": "Togo",
+    "TK": "Tokelau",
+    "TO": "Tonga",
+    "TT": "Trinidad and Tobago",
+    "TN": "Tunisia",
+    "TR": "Turkey",
+    "TM": "Turkmenistan",
+    "TC": "Turks and Caicos Islands (the)",
+    "TV": "Tuvalu",
+    "UG": "Uganda",
+    "UA": "Ukraine",
+    "AE": "United Arab Emirates (the)",
+    "GB": "United Kingdom of Great Britain and Northern Ireland (the)",
+    "UM": "United States Minor Outlying Islands (the)",
+    "US": "United States of America (the)",
+    "UY": "Uruguay",
+    "UZ": "Uzbekistan",
+    "VU": "Vanuatu",
+    "VE": "Venezuela (Bolivarian Republic of)",
+    "VN": "Viet Nam",
+    "VG": "Virgin Islands (British)",
+    "VI": "Virgin Islands (U.S.)",
+    "WF": "Wallis and Futuna",
+    "EH": "Western Sahara",
+    "YE": "Yemen",
+    "ZM": "Zambia",
+    "ZW": "Zimbabwe",
+    "AX": "Åland Islands"
+};
+
+
 
 
 function SearchInfo() {
-fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
-    method: 'POST', 
-    body: 'grant_type=client_credentials&client_id=' + i + '&client_secret=' + secret,
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    },
-})
-    .then(response => response.json())
-    .then(data => {
-        var token = data['access_token']
-        var ACCESS_TOKEN = 'Bearer ' + token;
-        // console.log('Success:', data);
-    fetch("https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=US", {
+    console.log("The search info was selected")
+    fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
+        method: 'POST',
+        body: 'grant_type=client_credentials&client_id=' + id + '&client_secret=' + secret,
         headers: {
-            Authorization: ACCESS_TOKEN,
-            "Content-Type": 'application/json'
-        }
-    })
-
-  .then(response => response.json())
-  .then(data => {
-      var nameEl = data['data']['area']['name']
-      var areavaxVal = data['data']['areaVaccinated']['0']['percentage'];
-      var areavax2Val = data['data']['areaVaccinated']['1']['percentage'];
-      var casesVal = data['data']['diseaseCases']['confirmed'];
-      var risksVal = data['data']['diseaseRiskLevel'];
-      var typeVal = data['data']['type'];
-      var summaryVal = data['data']['summary'];
-
-      areaEl.innerHTML = nameEl;
-      areavaxEl.innerHTML = areavaxVal + ' %';
-      areavax2El.innerHTML = areavax2Val + ' %';
-      casesEl.innerHTML = casesVal;
-      riskEl.innerHTML = risksVal;
-      typeEl.innerHTML = typeVal;
-      summaryEl.innerHTML = summaryVal;
-
-        // function getsendEmail (event){
-        fetch("https://submit-form.com/zj6diEJw", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: JSON.stringify({
-            message: "Hello, World",
-            'Country Name': nameEl,
-            'percentage with 1 dose': areavaxVal + ' %',
-            'percentage fully vaxed': areavax2Val + ' %',
-        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            var token = data['access_token']
+            var ACCESS_TOKEN = 'Bearer ' + token;
+            console.log('Success:', data);
+            fetch("https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=US", {
+                headers: {
+                    Authorization: ACCESS_TOKEN,
+                    "Content-Type": 'application/json'
+                }
+            })
+
+                .then(response => response.json())
+                .then(data => {
+                    var nameEl = data['data']['area']['name']
+                    var areavaxVal = data['data']['areaVaccinated']['0']['percentage'];
+                    var areavax2Val = data['data']['areaVaccinated']['1']['percentage'];
+                    var casesVal = data['data']['diseaseCases']['confirmed'];
+                    var risksVal = data['data']['diseaseRiskLevel'];
+                    var typeVal = data['data']['type'];
+                    var summaryVal = data['data']['summary'];
+
+                    areaEl.innerHTML = nameEl;
+                    areavaxEl.innerHTML = areavaxVal + ' %';
+                    areavax2El.innerHTML = areavax2Val + ' %';
+                    casesEl.innerHTML = casesVal;
+                    riskEl.innerHTML = risksVal;
+                    typeEl.innerHTML = typeVal;
+                    summaryEl.innerHTML = summaryVal;
+
+                    // function getsendEmail (event){
+                    fetch("https://submit-form.com/zj6diEJw", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Accept: "application/json",
+                        },
+                        body: JSON.stringify({
+                            message: "COVID-19 Information",
+                            'Country Name': nameEl,
+                            'percentage with 1 dose': areavaxVal + ' %',
+                            'percentage fully vaxed': areavax2Val + ' %',
+                        }),
+                    })
+                        .then(function (response) {
+                            console.log(response);
+                        })
+                        .catch(function (error) {
+                            console.error(error);
+
+                        });
+                    console.log('Success:', data)
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+
+                });
         })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.error(error);
-            
-        });
-    console.log('Success:', data)
-    },)
-  .catch((error) => {
-    console.error('Error:', error);
-    
-});})}
+}
 
-// var send = document.querySelector('#send');
-// send.addEventListener("click",sendEmail());
 
-Handlebars.registerHelper("json", function (context) {
-    return JSON.stringify(context);
-});
 
-SearchInfo()
+// // var send = document.querySelector('#send');
+// // send.addEventListener("click",sendEmail());
+
+// Handlebars.registerHelper("json", function (context) {
+//     return JSON.stringify(context);
+// });
+
+
+countryEl.addEventListener("click", SearchInfo)
 
 
 
